243db295b3e4ae1d2dce3330a7f2bcb675cbab24
anushka <anushkanil1@gmail.com>
2024-01-30 18:48:09 -0800

    changes

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ let monText
 let dateTotal
 let dateText
 function cal(){
-    let date=document.getElementById('input-date').value
+    let date=(document.getElementById('input-date').value)
    let month=parseInt(document.getElementById('input-mon').value)
     let year =parseInt(document.getElementById('input-year').value)
     yearText=document.getElementById('year')
     monText=document.getElementById('month')
     dateText=document.getElementById('date')
-    if(date!==0 && date<31 && month!==0||month<13){
+    if(date<=0 && date<31 && month<=0 && month<13){
+        console.log("hello")
         if(dateNow<date){
             monTotal=(monNow-month)-1
             let pre=monNow-1
@@ -37,7 +38,9 @@ function cal(){
             totalYear=(yearNow-year)
             monTotal=monNow-month
         }
-    
+        document.getElementById('input-date').classList.remove('error')
+        document.getElementById('input-mon').classList.remove('error')
+        document.getElementById('input-year').classList.remove('error')
        yearText.innerText=totalYear
        monText.innerText=monTotal
        dateText.innerText=dateTotal
@@ -48,14 +51,34 @@ function cal(){
        month=0
        year=0
     }else{
-        
+        console.log(typeof(date))
+        console.log("qorking")
+        if(date<=0 || date>32 || date!==null){
+            if(date<=0 || date>31){
+                document.getElementById('error-date').innerHTML='Value needs to be between 1-31'
+            }else if(date == ""){
+                document.getElementById('error-date').innerHTML='Value is undefined'
+            }
             var errDate= document.getElementById('input-date')
             errDate.classList.add('error')
-            var p=document.createElement('p')
-            if()
-            p.innerText="Undifened"
-             var errDate= document.getElementById('input-mon')
-             errDate.classList.add('error')
+            
+        }
+        if(month===0 || month>12 || month!==null){
+            if(month<=0 || month>31){
+                document.getElementById('error-mon').innerHTML='Value needs to be between 1-12'
+            }else if(month===null){
+                document.getElementById('error-mon').innerHTML='Value is undefined'
+            }
+            var errDate= document.getElementById('input-mon')
+            errDate.classList.add('error')
+        }
+        if(year!==null){
+            var errDate= document.getElementById('input-year')
+            errDate.classList.add('error')
+        }
+           
+            
+            
     }
     
     
