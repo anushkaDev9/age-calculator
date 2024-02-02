d69e14b63fde02b637fce8469fd0cbcf0c764e0d
anushka <anushkanil1@gmail.com>
2024-02-01 22:04:04 -0800

    commit

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ let monText
 let dateTotal
 let dateText
 function cal(){
-    let date=(document.getElementById('input-date').value)
+    let date=parseInt(document.getElementById('input-date').value)
    let month=parseInt(document.getElementById('input-mon').value)
     let year =parseInt(document.getElementById('input-year').value)
     yearText=document.getElementById('year')
     monText=document.getElementById('month')
     dateText=document.getElementById('date')
-    if(date<=0 && date<31 && month<=0 && month<13){
+    
         console.log("hello")
         if(dateNow<date){
             monTotal=(monNow-month)-1
@@ -48,38 +48,12 @@ function cal(){
        document.getElementById('input-date').value=''
        document.getElementById('input-mon').value=''
        document.getElementById('input-year').value=''
-       month=0
-       year=0
-    }else{
-        console.log(typeof(date))
-        console.log("qorking")
-        if(date<=0 || date>32 || date!==null){
-            if(date<=0 || date>31){
-                document.getElementById('error-date').innerHTML='Value needs to be between 1-31'
-            }else if(date == ""){
-                document.getElementById('error-date').innerHTML='Value is undefined'
-            }
-            var errDate= document.getElementById('input-date')
-            errDate.classList.add('error')
-            
-        }
-        if(month===0 || month>12 || month!==null){
-            if(month<=0 || month>31){
-                document.getElementById('error-mon').innerHTML='Value needs to be between 1-12'
-            }else if(month===null){
-                document.getElementById('error-mon').innerHTML='Value is undefined'
-            }
-            var errDate= document.getElementById('input-mon')
-            errDate.classList.add('error')
-        }
-        if(year!==null){
-            var errDate= document.getElementById('input-year')
-            errDate.classList.add('error')
-        }
+     
+    
            
             
-            
-    }
+     
+    
     
     
 }
